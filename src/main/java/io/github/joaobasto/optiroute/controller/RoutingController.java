ad252ed4c92490609f733a161c4d47ace3ca82e9
João Basto <23194887+joaobasto@users.noreply.github.com>
2025-08-09 00:33:27 +0100

    Add OR tools solver, working successfully

--- src/main/java/io/github/joaobasto/optiroute/controller/RoutingController.java
+++ src/main/java/io/github/joaobasto/optiroute/controller/RoutingController.java
@@ -1,16 +1,109 @@
 package io.github.joaobasto.optiroute.controller;
 
+import com.google.ortools.Loader;
+import com.google.ortools.constraintsolver.*;
+import com.google.protobuf.Duration;
 import io.github.joaobasto.optiroute.dto.RoutingRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class RoutingController {
+    private static final int DEPOT_INDEX = 0;
 
     @PostMapping("/solve")
     public String solveCvrp(@RequestBody RoutingRequest request) {
-        // Just echo back for now
-        return "Demands: " + request.getDemands() +
-                ", Capacities: " + request.getVehicleCapacities();
+        Loader.loadNativeLibraries();
+
+        // Create Routing Index Manager
+        RoutingIndexManager manager =
+                new RoutingIndexManager(request.getDistanceMatrix().size(), request.getVehicleCapacities().size(), DEPOT_INDEX);
+
+        // Create Routing Model.
+        RoutingModel routing = new RoutingModel(manager);
+
+        // Create and register a transit callback.
+        final int transitCallbackIndex =
+                routing.registerTransitCallback((long fromIndex, long toIndex) -> {
+                    // Convert from routing variable Index to user NodeIndex.
+                    int fromNode = manager.indexToNode(fromIndex);
+                    int toNode = manager.indexToNode(toIndex);
+                    return request.getDistanceMatrix().get(fromNode).get(toNode);
+                });
+
+        // Define cost of each arc.
+        routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
+
+        // Add Capacity constraint.
+        final int demandCallbackIndex = routing.registerUnaryTransitCallback((long fromIndex) -> {
+            // Convert from routing variable Index to user NodeIndex.
+            int fromNode = manager.indexToNode(fromIndex);
+            return request.getDemands().get(fromNode);
+        });
+        boolean unused = routing.addDimensionWithVehicleCapacity(demandCallbackIndex,
+                0, // null capacity slack
+                request.getVehicleCapacities().stream().mapToLong(Integer::longValue).toArray(), // vehicle maximum capacities
+                true, // start cumul to zero
+                "Capacity");
+
+        // Setting first solution heuristic.
+        RoutingSearchParameters searchParameters =
+                main.defaultRoutingSearchParameters()
+                        .toBuilder()
+                        .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+                        .setLocalSearchMetaheuristic(LocalSearchMetaheuristic.Value.GUIDED_LOCAL_SEARCH)
+                        .setTimeLimit(Duration.newBuilder().setSeconds(1).build())
+                        .build();
+
+        // Solve the problem.
+        Assignment solution = routing.solveWithParameters(searchParameters);
+
+        // Print solution on console.
+        return printSolution(request, routing, manager, solution);
+    }
+
+    private String printSolution(
+            RoutingRequest routingRequest, RoutingModel routing, RoutingIndexManager manager, Assignment solution) {
+        StringBuilder stringBuilder = new StringBuilder();
+        // Solution cost.
+        stringBuilder.append("Objective: ");
+        stringBuilder.append(solution.objectiveValue());
+        stringBuilder.append(System.lineSeparator());
+
+        // Inspect solution.
+        long totalDistance = 0;
+        long totalLoad = 0;
+        for (int i = 0; i < routingRequest.getVehicleCapacities().size(); ++i) {
+            if (!routing.isVehicleUsed(solution, i)) {
+                continue;
+            }
+            long index = routing.start(i);
+            stringBuilder.append("Route for Vehicle ").append(i).append(":");
+            stringBuilder.append(System.lineSeparator());
+            long routeDistance = 0;
+            long routeLoad = 0;
+            String route = "";
+            while (!routing.isEnd(index)) {
+                long nodeIndex = manager.indexToNode(index);
+                routeLoad += routingRequest.getDemands().get((int) nodeIndex);
+                route += nodeIndex + " Load(" + routeLoad + ") -> ";
+                long previousIndex = index;
+                index = solution.value(routing.nextVar(index));
+                routeDistance += routing.getArcCostForVehicle(previousIndex, index, i);
+            }
+            route += manager.indexToNode(routing.end(i));
+            stringBuilder.append(route);
+            stringBuilder.append(System.lineSeparator());
+            stringBuilder.append("Distance of the route: ").append(routeDistance).append("m");
+            stringBuilder.append(System.lineSeparator());
+            totalDistance += routeDistance;
+            totalLoad += routeLoad;
+        }
+        stringBuilder.append("Total distance of all routes: " + totalDistance + "m");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("Total load of all routes: " + totalLoad);
+        stringBuilder.append(System.lineSeparator());
+
+        return stringBuilder.toString();
     }
 }
--- src/main/java/io/github/joaobasto/optiroute/dto/RoutingRequest.java
+++ src/main/java/io/github/joaobasto/optiroute/dto/RoutingRequest.java
@@ -1,10 +1,39 @@
 package io.github.joaobasto.optiroute.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public class RoutingRequest {
+
+    @Schema(description = "List of demands. First one is the depot and is expected to be 0",
+            example = "[0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8]")
     private List<Integer> demands;
+    @Schema(description = "List of vehicle capacities",
+            example = "[15, 15, 15, 15]")
     private List<Integer> vehicleCapacities;
+    @Schema(description = "Distance matrix - element in row i, column j, represents the distance from point i to point j. " +
+            "Point 0 is the depot.",
+            example = "[\n" +
+                    "  [0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468, 776, 662],\n" +
+                    "  [548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674, 1016, 868, 1210],\n" +
+                    "  [776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130, 788, 1552, 754],\n" +
+                    "  [696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822, 1164, 560, 1358],\n" +
+                    "  [582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708, 1050, 674, 1244],\n" +
+                    "  [274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514, 1050, 708],\n" +
+                    "  [502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514, 1278, 480],\n" +
+                    "  [194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662, 742, 856],\n" +
+                    "  [308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320, 1084, 514],\n" +
+                    "  [194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274, 810, 468],\n" +
+                    "  [536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730, 388, 1152, 354],\n" +
+                    "  [502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308, 650, 274, 844],\n" +
+                    "  [388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536, 388, 730],\n" +
+                    "  [354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342, 422, 536],\n" +
+                    "  [468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342, 0, 764, 194],\n" +
+                    "  [776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388, 422, 764, 0, 798],\n" +
+                    "  [662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536, 194, 798, 0]\n" +
+                    "]\n")
+    private List<List<Integer>> distanceMatrix;
 
     public List<Integer> getDemands() {
         return demands;
@@ -18,5 +47,13 @@ public class RoutingRequest {
     }
     public void setVehicleCapacities(List<Integer> vehicleCapacities) {
         this.vehicleCapacities = vehicleCapacities;
+    }
+
+    public List<List<Integer>> getDistanceMatrix() {
+        return distanceMatrix;
+    }
+
+    public void setDistanceMatrix(List<List<Integer>> distanceMatrix) {
+        this.distanceMatrix = distanceMatrix;
     }
 }
