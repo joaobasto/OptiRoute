ef13dd6b62c45cd2a0ce479251152c0ca9a6e35b
João Basto <23194887+joaobasto@users.noreply.github.com>
2025-08-08 23:03:44 +0100

    Add project skeleton

---
+++ src/main/java/io/github/joaobasto/optiroute/OptirouteApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaobasto.optiroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptirouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptirouteApplication.class, args);
+	}
+
+}
